6e9c35c540b6543407e40c74be282a784f1447c7
codebyMai <158331419+codebyMai@users.noreply.github.com>
2024-12-31 23:05:26 +0000

    fix issues with product template

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import Products, Shop
+from .views import Products, Shop, ProductDetail
 
 urlpatterns = [
 
     path('shop/', Shop.as_view(), name='shop'),
     path('', Products.as_view(), name='products'),
+    path('product/<int:pk>/', ProductDetail.as_view(), name='detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,23 +1,20 @@
 from django.shortcuts import render
 from .models import Product
-from django.views.generic import (
-ListView, DetailView)
-
-
-#def shop(request):
-   # """ Shop page view """
-    #return render(request,'shop.html')
+from django.views.generic import (ListView, DetailView)
 
 class Shop(ListView):
-    """List of quotes view"""
+    """Main Shop page"""
     template_name = "products/shop.html"
     model = Product
     context_object_name = 'products'
     paginate_by = 4
 
-#def products(request):
-   #return render(request, 'shop/products.html')
+def miniature(request):
+    products = Product.objects.filter(is_miniature=True).values()
 
+def featured(request):
+    products = Product.objects.filter(is_miniature=False).values()    
+  
 class Products(ListView):
     """List of products view"""
     template_name = "products/products.html"
@@ -25,6 +22,10 @@ class Products(ListView):
     context_object_name = 'products'
     
 
+class ProductDetail(DetailView):
+    """Detailed product view"""
+    model = Product
+    template_name = 'products/detail.html'
 
 
     
