a86b544e5b9b9b33ea70a19a598ab0b6f1e48cd6
codebyMai <158331419+codebyMai@users.noreply.github.com>
2025-01-03 13:54:56 +0000

    add shop template

--- products/views.py
+++ products/views.py
@@ -9,23 +9,31 @@ class Shop(ListView):
     context_object_name = 'products'
     paginate_by = 4
 
-def miniature(request):
-    products = Product.objects.filter(is_miniature=True).values()
-
-def featured(request):
-    products = Product.objects.filter(is_miniature=False).values()    
+def painting(request):
+    products = Product.objects.filter(is_miniature=False)   
   
+
 class Products(ListView):
     """List of products view"""
     template_name = "products/products.html"
     model = Product
     context_object_name = 'products'
-    
 
 class ProductDetail(DetailView):
     """Detailed product view"""
     model = Product
     template_name = 'products/detail.html'
 
+class Miniature(ListView):
+    products = Product.objects.filter(is_miniature=True)     
+    template_name= "products/products.html"
+    model = Product
+    context_object_name = 'miniature'
+
+class Painting(ListView):
+    products = Product.objects.filter(is_miniature=False)     
+    template_name= "products/products.html"
+    model = Product
+    context_object_name = 'painting'    
 
     
