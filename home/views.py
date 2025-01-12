dfbd7f0304575d5bd068281d6c372277af990dcc
codebyMai <158331419+codebyMai@users.noreply.github.com>
2025-01-12 19:21:10 +0000

    add galllery template

--- home/views.py
+++ home/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     """ Index page view """
-
     return render(request, 'home/index.html')
+
+def about(request):
+    """ About page view """
+    return render(request, 'home/about.html')    
