2a7ad0edabd0e8e3849f7388c99aecf256e5a753
codebyMai <158331419+codebyMai@users.noreply.github.com>
2025-01-15 14:06:22 +0000

    aet up whitenoise

--- artshop/settings.py
+++ artshop/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -166,10 +167,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/art-shop/static/',
-]
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
